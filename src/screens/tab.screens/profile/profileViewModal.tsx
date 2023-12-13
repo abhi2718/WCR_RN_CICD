77b359d3c29ec0fc568aa1510f3ad222ed9f6ae4
Abhishek Singh <abhiwebdev2718@gmail.com>
2023-12-13 12:14:49 +0530

    Error State

--- src/screens/tab.screens/profile/profileViewModal.tsx
+++ src/screens/tab.screens/profile/profileViewModal.tsx
@@ -5,7 +5,7 @@ import { ROUTES } from "../../../navigation";
 export const useViewModal = () => {
     const { user } = useSelector(({ userState }) => userState);
     const navigation = useNavigation();
-    const goToPreview = () => navigation.navigate(ROUTES.Preview);
+    const goToPreview  = () => navigation.navigate(ROUTES.Preview);
     const goToSetting = () => navigation.navigate(ROUTES.SettingScreen);
     const goToPreferences = () => navigation.navigate(ROUTES.Preferences);
     const goToEditProfile = () => navigation.navigate(ROUTES.EditProfile);
