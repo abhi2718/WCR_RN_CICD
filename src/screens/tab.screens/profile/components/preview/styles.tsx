587b3b950df185c58b2a4e6d926c1a234ba6fcf4
Vishalp001 <vishalpohekar1@gmail.com>
2023-12-15 14:15:08 +0530

    Style Preview page

--- src/screens/tab.screens/profile/components/preview/styles.tsx
+++ src/screens/tab.screens/profile/components/preview/styles.tsx
@@ -1,14 +1,139 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
+import {
+  fontSizes,
+  fontWeights,
+  fonts,
+} from '../../../../../infrastructure/theme/fonts';
+import { colors } from '../../../../../infrastructure/theme/colors';
+import { dimensions } from '../../../../../components/tools';
+import { sizes } from '../../../../../infrastructure/theme/sizes';
 
 export const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#fff",
-        padding:16
-    },
-    iconStyle: {
-        width: 20,
-        height: 20,
-        
-    }
-})
+  container: {
+    // paddingHorizontal: 16,
+  },
+  header: {
+    marginBottom: 16,
+    paddingHorizontal: 16,
+  },
+  closeIcon: {
+    width: 20,
+    height: 20,
+  },
+  headerText: {
+    fontSize: fontSizes.title,
+    color: colors.ui.text,
+    fontWeight: fontWeights.medium,
+  },
+  profilePic: {
+    width: dimensions.width,
+    height: dimensions.height - 120,
+    // borderRadius: 40,
+  },
+  pictures: {
+    width: dimensions.width,
+    height: dimensions.height - 150,
+  },
+  //  -----------
+
+  imageStyle: {
+    width: dimensions.width - sizes[3],
+    borderRadius: sizes[4],
+  },
+  nameRow: {
+    flex: 1,
+    position: 'absolute',
+    bottom: sizes[4],
+    left: sizes[4],
+  },
+  name: {
+    fontSize: fontSizes.h6,
+    color: colors.ui.white,
+    fontWeight: fontWeights.bold,
+    fontFamily: fonts.body,
+  },
+  gradient: {
+    height: '100%',
+    width: '100%',
+  },
+  badge: {
+    height: sizes[8],
+    width: sizes[5],
+  },
+  userInfo: {
+    marginTop: sizes[2],
+    paddingHorizontal: 16,
+  },
+  vitalSigns: {
+    paddingHorizontal: 16,
+  },
+  userInfoRow: {
+    marginTop: sizes[1] / 2,
+  },
+  userInfoIcon: {
+    height: sizes[5],
+    width: sizes[5],
+    overflow: 'visible',
+    resizeMode: 'contain',
+  },
+  userInfoText: {
+    color: colors.ui.text,
+    fontWeight: fontWeights.medium,
+    fontSize: fontSizes.text,
+  },
+  headingText: {
+    marginTop: sizes[2],
+    color: colors.ui.text,
+    fontWeight: fontWeights.bold,
+    fontSize: fontSizes.text,
+    fontFamily: fonts.body,
+  },
+  vitalSignsChips: {
+    marginVertical: sizes[2],
+    flexWrap: 'wrap',
+  },
+  chip: {
+    backgroundColor: colors.ui.deckChipBgColor,
+    paddingVertical: sizes[1],
+    paddingHorizontal: sizes[2],
+    borderRadius: sizes[4],
+  },
+  chipIcon: {
+    height: sizes[5],
+    width: sizes[5],
+    overflow: 'visible',
+    resizeMode: 'contain',
+  },
+  chipText: {
+    color: colors.ui.text,
+    fontSize: fontSizes.button,
+  },
+  aboutText: {
+    color: colors.ui.text,
+    fontWeight: fontWeights.regular,
+    fontSize: fontSizes.button,
+    marginTop: sizes[1],
+  },
+  inBtwnText: {
+    marginVertical: sizes[1],
+    paddingHorizontal: 16,
+  },
+  blockReportText: {
+    textAlign: 'center',
+    marginVertical: sizes[2],
+    fontWeight: fontWeights.semiBold,
+    fontSize: fontSizes.title,
+  },
+  footerIconRow: {
+    marginVertical: sizes[5],
+  },
+  footerIcon: {
+    height: sizes[12],
+    width: sizes[12],
+    overflow: 'visible',
+    resizeMode: 'contain',
+  },
+  mainContainer: {
+    marginBottom: 75,
+  },
+});
