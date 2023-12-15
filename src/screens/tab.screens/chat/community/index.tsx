0a4d8b8d192e2dd589a4973206083f2eda28f562
Abhishek Singh <abhiwebdev2718@gmail.com>
2023-12-15 14:04:44 +0530

    new icon added

--- src/screens/tab.screens/chat/community/index.tsx
+++ src/screens/tab.screens/chat/community/index.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import {View, Image,Text} from 'react-native';
-//import {CometChatConversationsWithMessages} from '../../../../cometChat/src';
+import {CometChatConversationsWithMessages} from '../../../../cometChat/src';
 import {useViewModal} from './useViewModal';
 import SwitchButton from './components/switchButton';
 import AllGroups from './components/allGroups';
 import {Column, Logo, Row,ScreenWrapper} from '../../../../components/tools';
 import {styles} from './styles';
-import { CometChatConversationsWithMessages } from '@cometchat/chat-uikit-react-native';
+//import { CometChatConversationsWithMessages } from '@cometchat/chat-uikit-react-native';
 export default function CommunityChat() {
   const {state, setState,count} = useViewModal();
   return (
@@ -33,7 +33,7 @@ export default function CommunityChat() {
         <SwitchButton stage={state} setStage={setState} />
       </View>
       {state === 0 ? <CometChatConversationsWithMessages
-       // isUserWindow={false}
+        isUserWindow={false}
       /> : <AllGroups />}
     </View>
     </ScreenWrapper>
