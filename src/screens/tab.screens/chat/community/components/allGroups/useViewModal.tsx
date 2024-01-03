f5924cf88cccaf7aaabc65820de4dbc58e30ee36
Abhishek Singh <abhiwebdev2718@gmail.com>
2024-01-03 18:48:37 +0530

    match

--- src/screens/tab.screens/chat/community/components/allGroups/useViewModal.tsx
+++ src/screens/tab.screens/chat/community/components/allGroups/useViewModal.tsx
@@ -1,9 +1,26 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { CometChat } from '@cometchat/chat-sdk-react-native';
-
+import { UpdateUserDetailsRepository } from '../../../../../../repository/pregisterFlow.repo';
+import { useDispatch, useSelector } from 'react-redux';
+import { addUser } from '../../../../../../store/reducers/user.reducer';
 export const useViewModal = () => {
   const [groups, setGroups] = useState<CometChat.Group[] | []>([]);
   const [loading, setLoading] = useState(false);
+  const { user } = useSelector(({ userState }) => userState);
+  const updateUserDetailsRepository = useMemo(
+    () => new UpdateUserDetailsRepository(),
+    [],
+  );
+  const [isModalVisible, setisModalVisible] = useState(
+    user?.communityInfoModalPoppedup ? user?.communityInfoModalPoppedup : false,
+  );
+  const dispatch = useDispatch();
+  const closeModal = (isWantToUpdateModalStatus: boolean) => {
+    if (isWantToUpdateModalStatus) {
+      updateCommunityInfoModalPoppedup();
+    }
+    setisModalVisible(true);
+  };
   const groupListRequestBuilder = async (
     searchKey = '',
     showLoader: boolean,
@@ -43,11 +60,40 @@ export const useViewModal = () => {
       setGroups(updatedGroups);
     } catch (error) {}
   };
+
+  const updateCommunityInfoModalPoppedup = async () => {
+    try {
+      const data = await updateUserDetailsRepository.updateUserDetails(
+        user._id,
+        {
+          update: {
+            communityInfoModalPoppedup: true,
+          },
+        },
+      );
+      const token = user.token;
+      const userInfo = token
+        ? {
+            user: {
+              ...data,
+              token,
+            },
+          }
+        : {
+            user: {
+              ...data,
+            },
+          };
+      dispatch(addUser(userInfo));
+    } catch (error) {}
+  };
   return {
     groups,
     handleTextChange,
     loading,
     handleJoinGroup,
     groupListRequestBuilder,
+    closeModal,
+    isModalVisible,
   };
 };
