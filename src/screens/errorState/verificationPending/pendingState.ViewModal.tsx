23401dd163b45938a71bc2341a99aba037eabab7
Shazia Ahmad <shaziaahmad010@gmail.com>
2024-01-04 16:52:44 +0530

    on refresh get the api call

--- src/screens/errorState/verificationPending/pendingState.ViewModal.tsx
+++ src/screens/errorState/verificationPending/pendingState.ViewModal.tsx
@@ -1,19 +1,36 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { ScreenParams } from '../../../types/services.types/firebase.service';
 import { useSelector } from 'react-redux';
 import { ROUTES } from '../../../navigation';
+import { UpdateUserDetailsRepository } from '../../../repository/pregisterFlow.repo';
 
 export const usePandingStateViewModal = (props: ScreenParams) => {
-  const {navigation} = props; 
+  const updateUserDetailsRepository = new UpdateUserDetailsRepository();
+  const { navigation } = props;
   const { user } = useSelector((state: any) => state.userState);
-  const state = user?.verification?.status
-  const isFormSubmitted = user?.verificationId?.submitted
+  const state = user?.verification?.status;
+  const isFormSubmitted = user?.verificationId?.submitted;
+
+  useEffect(() => {
+    const me = async () => {
+      try {
+        const userdata = await updateUserDetailsRepository.me(user._id);
+        if (userdata.user.verification.status === 'Verified') {
+          navigation.navigate(ROUTES.Tab);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    me();
+  }, []);
 
   const navigateToGender = () => {
     navigation.navigate(ROUTES.Gender);
   };
-  if(user?.verification?.status==='Verified'){
+  if (user?.verification?.status === 'Verified') {
     navigation.navigate(ROUTES.Tab);
   }
-  return {state,isFormSubmitted,navigateToGender};
+  return { state, isFormSubmitted, navigateToGender };
 };
